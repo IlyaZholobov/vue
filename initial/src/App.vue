936b563d8c882db90df6406b4414f65346091bac
IlyaZholobov <ilyazh47@gmail.com>
2023-04-30 16:00:27 +0300

    hometask 1

--- initial/src/App.vue
+++ initial/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="app">
     <MyComponent />
+    <HomeTask />
   </div>
 </template>
 
 <script>
 import MyComponent from './components/Mycomponent.vue'
+import HomeTask from './components/HomeTask.vue'
 
 export default {
   name: 'App',
   components: {
-    MyComponent
+    MyComponent,
+    HomeTask
   }
 }
 </script>
---
+++ initial/src/components/HomeTask.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="root">
+        <p>
+            Введите кол-во продуктов:
+        </p>
+        <button @click="minusClick()">-</button>
+        <input type="number" :value="count" @change="changeInput($event.target.value)" max="10" min="1">
+        <button @click="plusClick()">+</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeTask',
+    data() {
+        return {
+            count: 1
+        }
+    },
+    methods: {
+        changeInput(value) {
+            this.count = parseInt(value)
+        },
+        minusClick() {
+            if (this.count != 1) {
+                this.count -= 1
+            }
+        },
+        plusClick() {
+            if (this.count != 10) {
+                this.count += 1
+            }
+        }
+    }
+
+}
+</script>
