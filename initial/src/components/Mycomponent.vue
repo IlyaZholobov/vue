c03fe49e99a986dcb70e1443bf4e864a76e1cb47
IlyaZholobov <ilyazh47@gmail.com>
2023-04-30 15:16:22 +0300

    lesson 1

---
+++ initial/src/components/Mycomponent.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="root">
+        <label>
+            Введите email:
+            <input type="email" :value="email" @input="emailInput($event.target.value)" />
+        </label>
+        <label>
+            Введите password:
+            <input type="password" :value="password" @input="passwordInput($event.target.value)" />
+        </label>
+    </div>
+    <p>email: {{ email }}</p>
+    <p>password: {{ password }}</p>
+</template>
+
+<script>
+export default {
+    name: 'MyComponent',
+    data() {
+        return {
+            email: 'example@mail.ru',
+            password: '1231243'
+        }
+    },
+    methods: {
+        emailInput(value) {
+            this.email = value
+        },
+        passwordInput(value) {
+            this.password = value
+        }
+    }
+}
+
+</script>
